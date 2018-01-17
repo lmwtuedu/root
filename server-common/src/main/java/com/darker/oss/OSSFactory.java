e25567604f12721d5b2591de7060b8b21efd9fa0
darker <lmwtuedu@163.com>
2018-01-17 15:23:09 +0800

    Delete io.server io.renren
    From io.server io.renren to com.darker

---
+++ server-common/src/main/java/com/darker/oss/OSSFactory.java
@@ -0,0 +1,40 @@
+package com.darker.oss;
+
+import com.darker.service.SysConfigService;
+import com.darker.utils.ConfigConstant;
+import com.darker.utils.Constant;
+import com.darker.utils.SpringContextUtils;
+import com.darker.service.SysConfigService;
+import com.darker.utils.ConfigConstant;
+import com.darker.utils.Constant;
+import com.darker.utils.SpringContextUtils;
+
+/**
+ * 文件上传Factory
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-26 10:18
+ */
+public final class OSSFactory {
+    private static SysConfigService sysConfigService;
+
+    static {
+        OSSFactory.sysConfigService = (SysConfigService) SpringContextUtils.getBean("sysConfigService");
+    }
+
+    public static CloudStorageService build(){
+        //获取云存储配置信息
+        CloudStorageConfig config = sysConfigService.getConfigObject(ConfigConstant.CLOUD_STORAGE_CONFIG_KEY, CloudStorageConfig.class);
+
+        if(config.getType() == Constant.CloudService.QINIU.getValue()){
+            return new QiniuCloudStorageService(config);
+        }else if(config.getType() == Constant.CloudService.ALIYUN.getValue()){
+            return new AliyunCloudStorageService(config);
+        }else if(config.getType() == Constant.CloudService.QCLOUD.getValue()){
+            return new QcloudCloudStorageService(config);
+        }
+
+        return null;
+    }
+
+}
---
+++ server-web/src/main/java/com/darker/dao/SysOssDao.java
@@ -0,0 +1,15 @@
+package com.darker.dao;
+
+
+import com.darker.entity.SysOssEntity;
+
+/**
+ * 文件上传
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-25 12:13:26
+ */
+public interface SysOssDao extends BaseDao<SysOssEntity> {
+	
+}
